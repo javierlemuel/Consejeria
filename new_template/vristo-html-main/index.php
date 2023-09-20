a532ff0602ded4ecf56c63b8fa11416a58f3d6b2
Natasha Ramos <natasha.ramos5@upr.edu>
2023-09-20 17:18:11 -0400

    fixed for loops

--- new_template/vristo-html-main/index.php
+++ new_template/vristo-html-main/index.php
@@ -374,6 +374,7 @@ function readJson($path)
                             <!-- start course lists -->
                             <div id="coursesSection">
                                 <?php
+                                //crea array con index "categoria" (categoria de cursos) y la data es el path del json dond se encuentran los cursos
                                 $courseCategories = array(
                                     "recomendadas" => "recommendedCourses.json",
                                     "concentracion" => "ccomCourses.json",
@@ -386,6 +387,7 @@ function readJson($path)
                                 //variable to control that only the first tab in the accoridon is open by default
                                 $tab = 1;
 
+                                //iterate through categories and create a new tab for the accordion 
                                 foreach ($courseCategories as $category  => $jsonPath) {
                                     //uppercasa category name
                                     $category = strtoupper($category);
@@ -415,15 +417,12 @@ function readJson($path)
 
                                     //create rows with the course info
                                     foreach ($allCourses as $course) {
-                                        $courseCode = $course['courseCode'];
-                                        $courseName = $course['name'];
-                                        $credits = $course['credits'];
                                         echo "
                                                 <tr>
                                                     <td><input type=\"checkbox\" class=\"form-checkbox\" /></td>
-                                                    <td>$courseCode</td>
-                                                    <td>$courseName</td>
-                                                    <td>$credits</td>                                                            
+                                                    <td>" . $course['courseCode'] . "</td>
+                                                    <td>" . $course['name'] . "</td>
+                                                    <td>" . $course['credits'] . "</td>                                                            
                                                 </tr>                                              
                                         ";
                                     }
@@ -534,36 +533,6 @@ function readJson($path)
                         }
                     }
                 },
-
-                messages: [{
-                        id: 1,
-                        image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-success-light dark:bg-success text-success dark:text-success-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg></span>',
-                        title: 'Congratulations!',
-                        message: 'Your OS has been updated.',
-                        time: '1hr',
-                    },
-                    {
-                        id: 2,
-                        image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-info-light dark:bg-info text-info dark:text-info-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg></span>',
-                        title: 'Did you know?',
-                        message: 'You can switch between artboards.',
-                        time: '2hr',
-                    },
-                    {
-                        id: 3,
-                        image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-danger-light dark:bg-danger text-danger dark:text-danger-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span>',
-                        title: 'Something went wrong!',
-                        message: 'Send Reposrt',
-                        time: '2days',
-                    },
-                    {
-                        id: 4,
-                        image: '<span class="grid place-content-center w-9 h-9 rounded-full bg-warning-light dark:bg-warning text-warning dark:text-warning-light"><svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">    <circle cx="12" cy="12" r="10"></circle>    <line x1="12" y1="8" x2="12" y2="12"></line>    <line x1="12" y1="16" x2="12.01" y2="16"></line></svg></span>',
-                        title: 'Warning',
-                        message: 'Your password strength is low.',
-                        time: '5days',
-                    },
-                ],
 
                 removeMessage(value) {
                     this.messages = this.messages.filter((d) => d.id !== value);
